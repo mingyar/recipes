3066bd5fc0617e7b676ae2f8daa3116f45ccd85a
Mingyar Furtado <mingyar.furtado@gmail.com>
2019-01-18 11:00:07 -0200

    Course Project - The Basics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is siply a test', 'https://toriavey.com/images/2015/04/Brisket-6-1-720x960.jpg')
+  ];
 
   constructor() { }
 
